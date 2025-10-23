a74a0293ebd5c8d37d2b86c9ad75c05f5e33882a
ABHIRAJ GAUTAM <abhirajgautam28@gmail.com>
2025-10-23 21:21:40 +0530

    Add basic end-to-end navigation tests with Playwright.

--- src/setupTests.js
+++ src/setupTests.js
@@ -20,3 +20,42 @@ if (typeof global.IntersectionObserver === 'undefined') {
 		unobserve() { }
 	}
 }
+
+// Mock canvas methods used by Demo canvas in jsdom environment
+if (typeof HTMLCanvasElement !== 'undefined') {
+	if (typeof HTMLCanvasElement.prototype.getContext === 'undefined') {
+		HTMLCanvasElement.prototype.getContext = function () {
+			// Return a minimal 2D context mock
+			return {
+				scale: () => {},
+				beginPath: () => {},
+				moveTo: () => {},
+				lineTo: () => {},
+				stroke: () => {},
+				clearRect: () => {},
+				fillRect: () => {},
+				strokeStyle: '#000',
+				lineWidth: 1,
+				lineCap: 'round',
+				lineJoin: 'round',
+			}
+		}
+	}
+
+	// Provide a sensible getBoundingClientRect used by Demo canvas resize logic
+	if (typeof HTMLCanvasElement.prototype.getBoundingClientRect === 'undefined') {
+		HTMLCanvasElement.prototype.getBoundingClientRect = function () {
+			return { width: 600, height: 360, left: 0, top: 0 }
+		}
+	}
+}
+
+// Lightweight lottie-web stub to avoid loading full animation runtime in tests
+if (typeof global.lottie === 'undefined') {
+	global.lottie = {
+		loadAnimation: (opts = {}) => {
+			// return a fake animation controller with a destroy method
+			return { destroy: () => {} }
+		}
+	}
+}
